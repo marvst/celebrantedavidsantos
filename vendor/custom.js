c9649d5337b1920a6922572ad8c39118a0d717c7
Marcos Vinícius <marcosvst98@gmail.com>
2020-10-06 19:46:55 -0300

    first commit

---
+++ vendor/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+});
+
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+
+
+
